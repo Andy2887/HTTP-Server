8441a22f7ad20b8f3148b860c8dd00f9696bffaa
Andy2887 <yuanliheng05@gmail.com>
2025-08-27 20:20:56 -0400

    added comments

--- src/server.cpp
+++ src/server.cpp
@@ -16,22 +16,36 @@ int main(int argc, char **argv) {
   // You can use print statements as follows for debugging, they'll be visible when running tests.
   std::cout << "Logs from your program will appear here!\n";
 
-  // Uncomment this block to pass the first stage
-  //
+  // Create a socke and return the id
+  // AF_INET: the socket will use TCP
+  // SOCK_STREAM: indicate the socket will use a stream protocol
+  // 0: protocl number, 0 means use the default protocl
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
    std::cerr << "Failed to create server socket\n";
    return 1;
   }
   
-  // Since the tester restarts your program quite often, setting SO_REUSEADDR
-  // ensures that we don't run into 'Address already in use' errors
+  
   int reuse = 1;
+
+  //
+  // setsockopt: configuring certain behaviors or properties of the socket
+  // SOL_SOCKET: specifies that option is at socket level
+  // SO_REUSEADDR: allows the socket to bind to an address that is in a TIME_WAIT state
+  //
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
-  
+
+  //
+  // sockarr_in: a struct for setting the address information for binding the socket
+  // sin_family specifies the address family (IPv4).
+  // sin_addr.s_addr sets the IP address to any local address.
+  // sin_port sets the port number
+  // htons(): converts a 16-bit integer from host byte order to network byte order.
+  //
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
@@ -43,6 +57,11 @@ int main(int argc, char **argv) {
   }
   
   int connection_backlog = 5;
+
+  //
+  // The listen function in C++ is typically used in network programming to mark a socket as a passive socket that will accept incoming connection requests. 
+  // It takes two parameters: the socket file descriptor and the backlog, which specifies the maximum number of pending connections the queue can hold.
+  // 
   if (listen(server_fd, connection_backlog) != 0) {
     std::cerr << "listen failed\n";
     return 1;
